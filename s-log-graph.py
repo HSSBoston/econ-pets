9c618319b527832ee200cc7441c6ac0791cedef7
hssboston <hanna.s.suzuki@gmail.com>
2025-09-01 17:00:46 -0400

    graphs updated

--- s-log-graph.py
+++ s-log-graph.py
@@ -11,7 +11,7 @@ betaVals = [3/4, 2/3, 1/2, 1/3, 1/4]
 for beta in betaVals:
     for s in range(1, xMax+1):
         xVals.append(s)
-        yVals.append( math.log(s ** beta, math.e) )
+        yVals.append( math.log(math.e * (s ** beta), math.e) )
     plt.plot(xVals, yVals)
     for x, y in zip(xVals, yVals):
         plt.text(x, y, str(round(y,1)), ha="center", va="bottom")    
@@ -25,6 +25,6 @@ for beta in betaVals:
 
 plt.xlim(0, 17)
 plt.xlabel("s")
-plt.ylabel("ln(s^beta)")
+plt.ylabel("ln(e * s^beta)")
 plt.grid(True)
 plt.show()
--- v-curves.py
+++ v-curves.py
@@ -1,12 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-wVals     = [50, 75, 100, 125, 150, 175, 200, 225, 250, 275, 300]
+plt.figure(figsize=(6.4, 4.8*2.5))
+
+wVals     = [50, 75, 100, 125, 150, 175, 200]
 alphaVals = [7.5, 10, 12.5, 15, 17.5, 20]
 betaVals  = [1/2]
 vVals     = []
-
-plt.figure(figsize=(6.4*1.5, 4.8*2))
 
 for alpha in alphaVals:
     for beta in betaVals:
@@ -24,7 +24,48 @@ for alpha in alphaVals:
                  va="bottom")
         vVals = []
 
-plt.xlim(0, 310)
+wVals     = [50, 75, 100, 125, 150, 175, 200]
+alphaVals = [7.5, 10, 12.5, 15, 17.5, 20]
+betaVals  = [3/4]
+vVals     = []
+
+for alpha in alphaVals:
+    for beta in betaVals:
+        for w in wVals:
+            v = ( alpha * (1 - beta) * 16 * 60)/w
+            vVals.append(v)
+            print(round(v), "mins", "w:", w, "alpha:", alpha, "beta:", beta)
+        plt.plot(wVals, vVals)
+        for x, y in zip(wVals, vVals):
+            plt.text(x, y, str(round(y)), ha="center", va="bottom")
+        plt.text(1,
+                 vVals[0]-4,
+                 "α=" + str(alpha) + ", β=" + str(round(beta,2)),
+                 ha="left",
+                 va="bottom")
+        vVals = []
+
+wVals     = [50, 75, 100, 125, 150, 175, 200]
+alphaVals = [7.5, 10, 12.5, 15, 17.5, 20]
+betaVals  = [1/4]
+vVals     = []
+
+for alpha in alphaVals:
+    for beta in betaVals:
+        for w in wVals:
+            v = ( alpha * (1 - beta) * 16 * 60)/w
+            vVals.append(v)
+            print(round(v), "mins", "w:", w, "alpha:", alpha, "beta:", beta)
+        plt.plot(wVals, vVals)
+        for x, y in zip(wVals, vVals):
+            plt.text(x, y, str(round(y)), ha="center", va="bottom")
+        plt.text(1,
+                 vVals[0]+4,
+                 "α=" + str(alpha) + ", β=" + str(round(beta,2)),
+                 ha="left",
+                 va="bottom")
+        vVals = []
+plt.xlim(0, 210)
 plt.xlabel("w")
 plt.ylabel("v")
 plt.grid(True)
