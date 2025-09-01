f3ddf8f1bab9eca215afc73618b445d2c38ba2d5
hssboston <hanna.s.suzuki@gmail.com>
2025-09-01 07:47:55 -0400

    v

--- v-curves.py
+++ v-curves.py
@@ -1,28 +1,28 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-wList     = [50, 75, 100, 125, 150, 175, 200, 225, 250, 275, 300]
-alphaList = [7, 9, 11, 13, 15]
-betaList  = [1/2, 3/4]
-vList     = []
+wVals     = [50, 75, 100, 125, 150, 175, 200, 225, 250, 275, 300]
+alphaVals = [7.5, 10, 12.5, 15, 17.5, 20]
+betaVals  = [1/2]
+vVals     = []
 
 plt.figure(figsize=(6.4*1.5, 4.8*2))
 
-for alpha in alphaList:
-    for beta in betaList:
-        for w in wList:
-            v = (2 * alpha * (1 - beta) * 16 * 60)/w
-            vList.append(v)
+for alpha in alphaVals:
+    for beta in betaVals:
+        for w in wVals:
+            v = ( alpha * (1 - beta) * 16 * 60)/w
+            vVals.append(v)
             print(round(v), "mins", "w:", w, "alpha:", alpha, "beta:", beta)
-        plt.plot(wList, vList)
-#         for x, y in zip(wList, vList):
-#             plt.text(x, y, str(round(y)), ha="center", va="bottom")
-        plt.text(5,
-                 vList[0],
+        plt.plot(wVals, vVals)
+        for x, y in zip(wVals, vVals):
+            plt.text(x, y, str(round(y)), ha="center", va="bottom")
+        plt.text(1,
+                 vVals[0],
                  "α=" + str(alpha) + ", β=" + str(round(beta,2)),
                  ha="left",
                  va="bottom")
-        vList = []
+        vVals = []
 
 plt.xlim(0, 310)
 plt.xlabel("w")
