8fb3f8acc073076445bb63575cfe689de269a09c
hssboston <hanna.s.suzuki@gmail.com>
2025-09-03 16:28:58 -0400

    Update v-curves.py

--- v-curves.py
+++ v-curves.py
@@ -2,8 +2,8 @@ import matplotlib.pyplot as plt
 
 plt.figure(figsize=(6.4, 4.8*2.5))
 
-wVals     = [50, 75, 100, 125, 150, 175, 200]
-alphaVals = [7.5, 10, 12.5, 15, 17.5, 20]
+wVals     = [25, 50, 75, 100, 125, 150]
+alphaVals = [5, 10, 15, 20]
 betaVals  = [1/2]
 vVals     = []
 
@@ -18,13 +18,34 @@ for alpha in alphaVals:
             plt.text(x, y, str(round(y)), ha="center", va="bottom")
         plt.text(1,
                  vVals[0],
-                 "α=" + str(alpha) + ", β=" + str(round(beta,2)),
+                 "α=" + str(alpha) + "β=" + str(round(beta,2)),
                  ha="left",
                  va="bottom")
         vVals = []
 
-wVals     = [50, 75, 100, 125, 150, 175, 200]
-alphaVals = [7.5, 10, 12.5, 15, 17.5, 20]
+wVals     = [25, 50, 75, 100, 125, 150]
+alphaVals = [5, 10, 15, 20]
+betaVals  = [2/3]
+vVals     = []
+
+for alpha in alphaVals:
+    for beta in betaVals:
+        for w in wVals:
+            v = ( alpha * (1 - beta) * 16 * 60)/w
+            vVals.append(v)
+            print(round(v), "mins", "w:", w, "alpha:", alpha, "beta:", beta)
+        plt.plot(wVals, vVals)
+        for x, y in zip(wVals, vVals):
+            plt.text(x, y, str(round(y)), ha="center", va="bottom")
+        plt.text(1,
+                 vVals[0]-6,
+                 "α=" + str(alpha) + "β=" + str(round(beta,2)),
+                 ha="left",
+                 va="bottom")
+        vVals = []
+
+wVals     = [25, 50, 75, 100, 125, 150]
+alphaVals = [5, 10, 15, 20]
 betaVals  = [3/4]
 vVals     = []
 
@@ -38,34 +59,14 @@ for alpha in alphaVals:
         for x, y in zip(wVals, vVals):
             plt.text(x, y, str(round(y)), ha="center", va="bottom")
         plt.text(1,
-                 vVals[0]-4,
-                 "α=" + str(alpha) + ", β=" + str(round(beta,2)),
+                 vVals[0]+6,
+                 "α=" + str(alpha) + "β=" + str(round(beta,2)),
                  ha="left",
                  va="bottom")
         vVals = []
 
-wVals     = [50, 75, 100, 125, 150, 175, 200]
-alphaVals = [7.5, 10, 12.5, 15, 17.5, 20]
-betaVals  = [1/4]
-vVals     = []
-
-for alpha in alphaVals:
-    for beta in betaVals:
-        for w in wVals:
-            v = ( alpha * (1 - beta) * 16 * 60)/w
-            vVals.append(v)
-            print(round(v), "mins", "w:", w, "alpha:", alpha, "beta:", beta)
-        plt.plot(wVals, vVals)
-        for x, y in zip(wVals, vVals):
-            plt.text(x, y, str(round(y)), ha="center", va="bottom")
-        plt.text(1,
-                 vVals[0]+4,
-                 "α=" + str(alpha) + ", β=" + str(round(beta,2)),
-                 ha="left",
-                 va="bottom")
-        vVals = []
-
-plt.xlim(0, 210)
+#plt.xlim(0, 155)
+plt.xticks([0, 25, 50, 75, 100, 125, 150])
 plt.xlabel("w ($/hr)", fontsize=20)
 plt.ylabel("v' (minutes)", fontsize=20)
 plt.grid(True)
