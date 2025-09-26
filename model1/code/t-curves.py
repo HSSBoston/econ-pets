31b8d9ddb975eae984fc8743c5ae2d548b9f2ba2
hssboston <hanna.s.suzuki@gmail.com>
2025-09-25 20:33:26 -0400

    Update t-curves.py

--- model1/code/t-curves.py
+++ model1/code/t-curves.py
@@ -2,72 +2,29 @@ import matplotlib.pyplot as plt
 
 plt.figure(figsize=(6.4, 4.8*2.5))
 
-wVals     = [25, 50, 75, 100, 125, 150]
-alphaVals = [2.5, 5, 7.5, 10, 12.5, 15]
-betaVals  = [0.5]
-vVals     = []
+petExpenditureRatios = [0.05, 0.06, 0.07, 0.08, 0.09, 0.10]
+gammaVals  = [0.2, 0.3, 0.4, 0.5, 0.6]
+tVals     = []
 
-for alpha in alphaVals:
-    for beta in betaVals:
-        for w in wVals:
-            v = ( alpha * (1 - beta) * 16 * 60)/w
-            vVals.append(v)
-            print(round(v), "mins", "w:", w, "alpha:", alpha, "beta:", beta)
-        plt.plot(wVals, vVals)
-        for x, y in zip(wVals, vVals):
-            plt.text(x, y, str(round(y)), ha="center", va="bottom")
-        plt.text(1,
-                 vVals[0],
-                 "α=" + str(alpha) + "β=" + str(round(beta,2)),
-                 ha="left",
-                 va="bottom")
-        vVals = []
+for gammaVal in gammaVals:
+    for petExpRatio in petExpenditureRatios:
+        t = (petExpRatio * (1 - gammaVal) * 15 * 60)
+        tVals.append(t)
+        print(round(t), "mins", "alpha/w:", petExpRatio, "gamma:", gammaVal)
+    plt.plot(petExpenditureRatios, tVals)
+    for x, y in zip(petExpenditureRatios, tVals):
+        plt.text(x, y, str(round(y)), ha="center", va="bottom")
+#     plt.text(1,
+#              vVals[0],
+#              "α=" + str(alpha) + "β=" + str(round(beta,2)),
+#              ha="left",
+#              va="bottom")
+    tVals = []
 
-wVals     = [25, 50, 75, 100, 125, 150]
-alphaVals = [2.5, 5, 7.5, 10, 12.5, 15]
-betaVals  = [0.6]
-vVals     = []
-
-for alpha in alphaVals:
-    for beta in betaVals:
-        for w in wVals:
-            v = ( alpha * (1 - beta) * 16 * 60)/w
-            vVals.append(v)
-            print(round(v), "mins", "w:", w, "alpha:", alpha, "beta:", beta)
-        plt.plot(wVals, vVals)
-        for x, y in zip(wVals, vVals):
-            plt.text(x, y, str(round(y)), ha="center", va="bottom")
-        plt.text(1,
-                 vVals[0]-6,
-                 "α=" + str(alpha) + "β=" + str(round(beta,2)),
-                 ha="left",
-                 va="bottom")
-        vVals = []
-
-wVals     = [25, 50, 75, 100, 125, 150]
-alphaVals = [2.5, 5, 7.5, 10, 12.5, 15]
-betaVals  = [0.7]
-vVals     = []
-
-for alpha in alphaVals:
-    for beta in betaVals:
-        for w in wVals:
-            v = ( alpha * (1 - beta) * 16 * 60)/w
-            vVals.append(v)
-            print(round(v), "mins", "w:", w, "alpha:", alpha, "beta:", beta)
-        plt.plot(wVals, vVals)
-        for x, y in zip(wVals, vVals):
-            plt.text(x, y, str(round(y)), ha="center", va="bottom")
-        plt.text(1,
-                 vVals[0]+6,
-                 "α=" + str(alpha) + "β=" + str(round(beta,2)),
-                 ha="left",
-                 va="bottom")
-        vVals = []
 
 #plt.xlim(0, 155)
-plt.xticks([0, 25, 50, 75, 100, 125, 150])
-plt.xlabel("w ($/hr)", fontsize=20)
-plt.ylabel("v^*' (minutes)", fontsize=20)
+#plt.xticks([0, 25, 50, 75, 100, 125, 150])
+#plt.xlabel("w ($/hr)", fontsize=20)
+#plt.ylabel("v^*' (minutes)", fontsize=20)
 plt.grid(True)
 plt.show()
