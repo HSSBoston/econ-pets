4df07a8503a9cc6a3310c001711cdeba2fe71827
hssboston <hanna.s.suzuki@gmail.com>
2025-09-30 14:59:50 -0400

    rev

--- model1/code/aw-t-lines.py
+++ model1/code/aw-t-lines.py
@@ -5,13 +5,14 @@ import matplotlib.pyplot as plt
 petExpenditureRatios = [0.05, 0.06, 0.07, 0.08, 0.09, 0.10]
 gammaVals  = [0.2, 0.3, 0.4, 0.5, 0.6]
 tVals     = []
+gammaValsIndex = 0
 
 for gammaVal in gammaVals:
     for petExpRatio in petExpenditureRatios:
         t = (petExpRatio * (1 - gammaVal) * 15 * 60)
         tVals.append(t)
         print(round(t), "mins", "alpha/w:", petExpRatio, "gamma:", gammaVal)
-    plt.plot(petExpenditureRatios, tVals)
+    plt.plot(petExpenditureRatios, tVals, label=f"γ={gammaVals[gammaValsIndex]}")
     for x, y in zip(petExpenditureRatios, tVals):
         plt.text(x, y, str(round(y)), ha="center", va="bottom")
 #     plt.text(1,
@@ -19,12 +20,14 @@ for gammaVal in gammaVals:
 #              "α=" + str(alpha) + "β=" + str(round(beta,2)),
 #              ha="left",
 #              va="bottom")
+    gammaValsIndex += 1
     tVals = []
 
 
 #plt.xlim(0, 155)
 #plt.xticks([0, 25, 50, 75, 100, 125, 150])
 plt.xlabel("α/w", fontsize=20)
-plt.ylabel("Time Spent for a Pet (minutes)", fontsize=17)
+plt.ylabel("Time Spent for a Pet Care (min)", fontsize=17)
 plt.grid(True)
+plt.legend()
 plt.show()
