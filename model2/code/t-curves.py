7b561ad2b6d46c68a1004c853d5d2c4d7c2990ae
hssboston <hanna.s.suzuki@gmail.com>
2026-02-28 23:53:10 -0500

    Update t-curves.py

--- model2/code/t-curves.py
+++ model2/code/t-curves.py
@@ -2,23 +2,25 @@ import matplotlib.pyplot as plt
 
 # plt.figure(figsize=(6.4, 4.8*2.5))
 
+k = 0.1
 wVals     = [25, 50, 75]
-alphaVals = [2.5, 5, 7.5]
-gammaVals  = [0.3, 0.4, 0.5]
+alphaVals = [2.5, 3.5, 5]
+gammaVals  = [0.15, 0.3, 0.5]
 tVals     = []
 
+
 for alpha in alphaVals:
-    for beta in gammaVals:
+    for gamma in gammaVals:
         for w in wVals:
-            v = ( alpha * (1 - beta) * 15 * 60)/w
-            tVals.append(v)
-            print(round(v), "mins", "w:", w, "alpha:", alpha, "beta:", beta)
+            t = (alpha * (1 - gamma) - k)/w * 15 * 60
+            tVals.append(t)
+            print(round(t), "mins", "w:", w, "alpha:", alpha, "gamma:", gamma)
         plt.plot(wVals, tVals)
         for x, y in zip(wVals, tVals):
             plt.text(x, y, str(round(y)), ha="center", va="bottom")
         plt.text(5.1,
                  tVals[0],
-                 "α=" + str(alpha) + "γ=" + str(round(beta,2)),
+                 "α=" + str(alpha) + "γ=" + str(round(gamma,2)),
                  fontsize="11",
                  ha="left",
                  va="bottom")
