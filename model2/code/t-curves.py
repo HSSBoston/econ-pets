ada028f56e521a738d5c5f87265bb0774d1019e9
hssboston <hanna.s.suzuki@gmail.com>
2026-03-01 00:04:23 -0500

    Update t-curves.py

--- model2/code/t-curves.py
+++ model2/code/t-curves.py
@@ -12,13 +12,13 @@ tVals     = []
 for alpha in alphaVals:
     for gamma in gammaVals:
         for w in wVals:
-            t = (alpha * (1 - gamma) - k)/w * 15 * 60
+            t = (alpha * (1 - gamma) - k)/w * 14.2 * 60
             tVals.append(t)
             print(round(t), "mins", "w:", w, "alpha:", alpha, "gamma:", gamma)
         plt.plot(wVals, tVals)
         for x, y in zip(wVals, tVals):
             plt.text(x, y, str(round(y)), ha="center", va="bottom")
-        plt.text(5.1,
+        plt.text(15.1,
                  tVals[0],
                  "α=" + str(alpha) + "γ=" + str(round(gamma,2)),
                  fontsize="11",
@@ -27,7 +27,7 @@ for alpha in alphaVals:
         tVals = []
 
 #plt.xlim(0, 155)
-plt.xticks([5, 25, 50, 75])
+plt.xticks([15, 30, 40, 50, 60, 70])
 plt.xlabel("Hourly wage rate (w)", fontsize=20)
 # plt.yticks([10, 25, 50, 75, 100, 125, 150, 175, 200])
 plt.ylabel("Caregiving time per pet (min)", fontsize=20)
