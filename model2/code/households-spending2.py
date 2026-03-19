f4695ea6eab0c9d4e87d63dc0ae932d5406c5378
hssboston <hanna.s.suzuki@gmail.com>
2026-03-19 09:19:29 -0400

    Update households-spending2.py

--- model2/code/households-spending2.py
+++ model2/code/households-spending2.py
@@ -18,18 +18,42 @@ hhDf       = df.dropna(subset=["hh"])
 # Remove rows that have empty cells and make yy-spending data frame
 spendingDf = df.dropna(subset=["spending"])
 
-plt.figure(figsize=(9, 5))
+# plt.figure(figsize=(9, 5))
 
-plt.plot(hh_df["year"], hh_df["hh"],
-         marker="o", linewidth=2, label="hh")
+fig, ax1 = plt.subplots()
+ax2 = ax1.twinx()
 
-plt.plot(spending_df["year"], spending_df["spending"],
-         marker="s", linewidth=2, label="spending")
+# plt.plot(hh_df["year"], hh_df["hh"],
+#          marker="o", linewidth=2, label="hh")
+# 
+# plt.plot(spending_df["year"], spending_df["spending"],
+#          marker="s", linewidth=2, label="spending")
 
-plt.xlabel("Year")
-plt.ylabel("Value")
-plt.title("hh and spending over years")
-plt.legend()
-plt.grid(True)
-plt.tight_layout()
+ax1.plot(numOfHouseholdsWithPetsYears, numOfHouseholdsWithPets,
+         color="red", marker="o", linewidth=3, markersize=8)
+ax2.plot(everyYear, petCareSpending,
+         color="blue", marker="o", linewidth=3, markersize=8)
+
+ax1.set_xlabel("Years", fontsize=20)
+ax1.set_ylabel("# of Households w/ Pets (M)", color="red", fontsize=20)
+ax1.set_yticks(np.arange(70,101,5))
+ax2.set_ylabel("Pet Expenditure ($bn)", color="blue", fontsize=20)
+ax2.set_yticks(np.arange(50,151,10))
+
+ax1.tick_params(axis='both', labelsize=16)
+ax2.tick_params(axis='both', labelsize=16)
+
+# plt.tick_params(labelsize=16)
+# plt.rcParams["font.size"] = 16
+
 plt.show()
+
+
+
+# plt.xlabel("Year")
+# plt.ylabel("Value")
+# plt.title("hh and spending over years")
+# plt.legend()
+# plt.grid(True)
+# plt.tight_layout()
+# plt.show()
