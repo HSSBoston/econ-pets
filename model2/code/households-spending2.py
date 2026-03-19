440c280c5c83bb8bc9eb8724eac16792778a36ca
hssboston <hanna.s.suzuki@gmail.com>
2026-03-19 09:17:28 -0400

    Update households-spending2.py

--- model2/code/households-spending2.py
+++ model2/code/households-spending2.py
@@ -33,24 +33,3 @@ plt.legend()
 plt.grid(True)
 plt.tight_layout()
 plt.show()
-
-# 
-# # Plot
-# plt.figure(figsize=(8, 5))
-# 
-# plt.plot(df["year"], df["hh"], marker='o', label="Households (hh)")
-# plt.plot(df["year"], df["spending"], marker='o', label="Spending")
-# 
-# # Labels and title
-# plt.xlabel("Year")
-# plt.ylabel("Value")
-# plt.title("Households and Spending Over Time")
-# 
-# # Legend
-# plt.legend()
-# 
-# # Grid (optional but nice)
-# plt.grid(True)
-# 
-# # Show plot
-# plt.show()
